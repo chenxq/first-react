b270a82ae9b361ac0d80b91047a09a6534743590
sarah.chen <sarah.chen@ringcentral.com>
2020-06-26 17:42:45 +0800

    Login form

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Customers from "./components/customers";
 import NotFound from "./components/notFound";
 import NavBar from "./components/common/navBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 // import NavBar from './components/navBar';
 
 class App extends Component {
@@ -47,8 +48,9 @@ class App extends Component {
       <React.Fragment>
         {/* <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} /> */}
         <main className="container">
-          <NavBar/>
+          <NavBar />
           <Switch>
+            <Route path="/login" component={LoginForm}></Route>
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
